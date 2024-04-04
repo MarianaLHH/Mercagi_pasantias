e1b4fb0e1b4639775dc4e5c2fbd7a8c45d1a888f
MarianaHH <laurahilari96@gmail.com>
2024-04-03 22:05:52 -0400

    holaa

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.getElementById("inputOrigen").addEventListener("input", function () {
   const texto = this.value;
   document.getElementById("inputDestino").value = texto;
+  //hola
+  var o = 56;
 });
