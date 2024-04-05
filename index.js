663a37107d10493d92754badcbb2d0f2c0714fd9
MarianaHH <laurahilari96@gmail.com>
2024-04-05 14:36:55 -0400

    ultima modificacion

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-document.getElementById("inputOrigen").addEventListener("input", function () {
+document.getElementById("inputOrigen1").addEventListener("input", function () {
   const texto = this.value;
-  document.getElementById("inputDestino").value = texto;
-  //hola
-  var o = 56;
+  document.getElementById("inputDestino1").value = texto;
+});
+document.getElementById("inputOrigin2").addEventListener("input", function () {
+  const texto = this.value;
+  document.getElementById("inputDestino2").value = texto;
+});
+
+document.getElementById("inputOrigen3").addEventListener("input", function () {
+  const texto = this.value;
+  document.getElementById("inputDestino3").value = texto;
 });
