f516699cec178255cd0f0601ab62cbe86cfbd64d
MarianaHH <laurahilari96@gmail.com>
2024-04-05 22:49:45 -0400

    prueba

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+//Input de entrada y salida
 document.getElementById("inputOrigen1").addEventListener("input", function () {
   const texto = this.value;
   document.getElementById("inputDestino1").value = texto;
 });
+
+//Input de entrada y salida
 document.getElementById("inputOrigin2").addEventListener("input", function () {
   const texto = this.value;
   document.getElementById("inputDestino2").value = texto;
 });
 
+//Input de entrada y salida
 document.getElementById("inputOrigen3").addEventListener("input", function () {
   const texto = this.value;
   document.getElementById("inputDestino3").value = texto;
